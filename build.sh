e48a3dc68a985c65f426c097d29710a6370f9df4
Fonthom <thomasf0n7a1n3@gmail.com>
2026-05-14 11:10:37 +0200

    docs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 src/main.py "https://github.com/Fonthom/static_site_generator"
+python3 src/main.py "/static_site_generator/"
 
 echo "Site built successfully into /docs folder!"
